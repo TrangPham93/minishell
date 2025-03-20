347ef5a38db10dd3b9687f7223991cc0462de5b3
Trang Pham <trpham@c2r1p2.hive.fi>
2025-03-20 16:23:42 +0200

    Create a prompt

--- includes/parser.h
+++ includes/parser.h
@@ -6,12 +6,15 @@
 /*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 16:41:06 by trpham            #+#    #+#             */
-/*   Updated: 2025/03/20 15:14:29 by trpham           ###   ########.fr       */
+/*   Updated: 2025/03/20 16:12:12 by trpham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/includes/libft.h"
+#include <signal.h>
 #include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
 typedef struct s_command
 {
--- srcs/parsing/main.c
+++ srcs/parsing/main.c
@@ -6,22 +6,39 @@
 /*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 13:57:54 by trpham            #+#    #+#             */
-/*   Updated: 2025/03/20 15:25:52 by trpham           ###   ########.fr       */
+/*   Updated: 2025/03/20 16:16:46 by trpham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "../includes/parser.h"
+#include "../includes/parser.h"
 #include <stdio.h>
-#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
 
 int	main(int ac, char *av[], char *env[])
 {
-	if (ac != 2)
+	char *prompt_name;
+
+	if (ac <= 0)
 		return (EXIT_FAILURE);
 	if (!*env)
 		return (EXIT_FAILURE);
 	(void) av;
-	printf("%s\n", env[10]);	
+	prompt_name = "minishell";
+	// signal(SIGUSR1, )
+	while (1)
+	{
+		// printf("%s>", prompt_name);	
+		// wait();
+		fputs("minishell>", stdout);
+		char *input = readline("minishell>");
+		add_history(input);
+		printf("%s", input);
+		free(input);
+	}
+	
+	
 
-	return (EXIT_SUCCESS);	
+	return (0);	
 }
